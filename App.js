94b691a4a11cdcc31a86b3ebd949a687e00a9062
cblsourav <cblsouravkumar@gmail.com>
2024-03-08 17:26:32 +0530

    new work

--- App.js
+++ App.js
@@ -5,21 +5,9 @@ import { Switch } from 'react-native-web';
 
 export default function App() {
 
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
-      <Button title="click" />
-      <StatusBar />
-      <Switch
-        trackColor={{ false: '#767577', true: '#81b0ff' }}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+    
     </View>
   );
 }
