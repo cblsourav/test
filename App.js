68f804da7cdcd6cea731ef5bafe771ebea85de8f
cblsourav <cblsouravkumar@gmail.com>
2024-03-11 12:38:52 +0530

    new changes

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Switch } from 'react-native-web';
